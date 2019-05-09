2f553199f20398443eac1dccff5e8a6be04fd537
dragonbjekic <bjekic.beograd@gmail.com>
2019-05-09 16:29:44 +0200

    rotating pipe

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 PRG = pipe_up
 CC = g++
 CFLAGS = -g -Wall -std=c++11
-GL = -lglut -lGL - lGLU
+GL = -lglut -lGL -lGLU
 
-PRG:$(CC) -o main.cpp $(PRG) $(CFLAGS) $(GL)
+$(PRG):
+	$(CC) -o $(PRG) main.cpp $(CFLAGS) $(GL)
 
 .PHONY: clean
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include<GL/gl.h>
 #include<GL/glu.h>
 #include<GL/glut.h>
+#include<cmath>
 
+int index = 0;
 void draw_grids(){
 glBegin(GL_LINES);
 			glColor3f(1,0,0);
@@ -22,42 +24,103 @@ glEnd();
 class Pipe{
 	private:
 		GLUquadric * qobj = gluNewQuadric();
-		GLfloat xtrans,ytrans,ztrans;
+		GLfloat xtrans,ytrans,ztrans,angleRange,angle;
 		
 	public:
-		Pipe(GLfloat x, GLfloat y, GLfloat z){
+		
+		bool direction=true;
+		Pipe(GLfloat x, GLfloat y, GLfloat z, GLfloat rang){
 			xtrans=x;
 			ytrans=y;
 			ztrans=z;
+			angleRange = rang;
+		}
+		void setAngle(GLfloat anglendo){
+			angle = anglendo;
+		}
+		GLfloat getRange(){
+			return angleRange;
+		}
+		GLfloat getCurrentAngle(){
+			return angle;
 		}
 		void drawPipe(){
 			glMatrixMode(GL_MODELVIEW);
 			glPushMatrix();
-				glTranslatef(xtrans,ytrans,ztrans);
+				glRotatef(angle,0,0,1);
+				
 				glRotatef(90,-1,0,0);
-				gluCylinder(qobj, .3,.3,1, 20,16);
+				glTranslatef(xtrans,ytrans,ztrans);
+				gluCylinder(qobj, .1,.1,1, 20,16);
 				
 			glPopMatrix();
 		}
+		GLfloat getx(){
+		    	return xtrans;
+		}
+		GLfloat gety(){
+		    	return ytrans;
+		}
+		GLfloat getz(){
+		    	return ztrans;
+		}
+		
 		
 };
+std::vector<Pipe> gandra;
+
+void drawTwoLines(GLfloat x, GLfloat y, GLfloat z, GLfloat range){
+	glPushMatrix();
+		glColor3f(0,1,1);
+		glBegin(GL_LINES);
+			glVertex3f(x,y,z);
+			glVertex3f(x+sin(range),y+cos(range),z);
+		glEnd();
+	glPopMatrix();
+}
+void rotation(int index){
+	GLfloat rang = gandra[index].getRange();
+	GLfloat xcord = gandra[index].getx();
+	GLfloat ycord = gandra[index].gety();
+	GLfloat zcord = gandra[index].getz();
+	drawTwoLines(xcord,ycord,zcord,rang);
+	GLfloat currAngle = gandra[index].getCurrentAngle();
+	if(abs((int)(currAngle))>90){
+		gandra[index].direction = !gandra[index].direction;
+		if (gandra[index].direction)
+			gandra[index].setAngle(currAngle+2);
+		else gandra[index].setAngle(currAngle-2);
+	}
+	else{
+		if (gandra[index].direction)
+			gandra[index].setAngle(currAngle+1);
+		else gandra[index].setAngle(currAngle-1);
+	}
+	
+	
+	glutPostRedisplay();
+	
+	//glutTimerFunc(100,rotation,index);
+	
+}
+
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	Pipe gandra = Pipe(1,0,0);
+	
+	
+	glutTimerFunc(10,rotation,index);
 	draw_grids();
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity(); 
 	gluPerspective(50.0, 1.0, 3.0, 7.0); 
+	
 	glMatrixMode(GL_MODELVIEW); 
 	glLoadIdentity(); 
 	gluLookAt(0,1,5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-			
-			
-			
-	gandra.drawPipe();
-	
-	
+	for(auto it: gandra)
+		it.drawPipe();
+	 
 	glutSwapBuffers();
 }
 
@@ -72,6 +135,7 @@ int main(int argc, char** argv){
 	glEnable(GL_DEPTH_TEST);
 	//callbacks
 	glutDisplayFunc(display);
+	gandra.push_back(Pipe(0,0,0,30));
 	
 	glutMainLoop();
 	return 0;
